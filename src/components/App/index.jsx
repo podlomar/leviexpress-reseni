882280affc3d74a6ecc329a79bfe6272eebd7084
Martin Podloucký <podlouckymartin@gmail.com>
2022-05-21 10:12:28 +0200

    Komponenta Reservation

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,12 +3,14 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from '../Reservation';
 
 export const App = () => (
   <BrowserRouter>
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/reservation" element={<Reservation />} />
     </Routes>
     <Footer />
   </BrowserRouter>
---
+++ src/components/Reservation/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Reservation = () => (
+  <div className="container">
+    <h2>Detail jízdenky</h2>
+  </div>
+);
