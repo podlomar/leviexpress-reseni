b3620b111365c06bc5dfb96656bb9f006eabc034
Martin Podloucký <podlouckymartin@gmail.com>
2022-05-21 10:09:03 +0200

    Základní routování

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
 
 export const App = () => (
-  <>
+  <BrowserRouter>
     <Header />
-    <Home />
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
     <Footer />
-  </>
+  </BrowserRouter>
 );
